42c32d91ba1267586db7d1eea91cda0813b5708d
Briana Bricher <bbricher@udallas.edu>
2019-08-18 12:52:46 -0500

    created working request for bands in town and spotify

--- liri.js
+++ liri.js
@@ -1,6 +1,10 @@
 require("dotenv").config();
 
+var moment = require('moment');
+
 var keys = require("./keys.js");
+
+var Spotify = require('node-spotify-api');
 
 var spotify = new Spotify(keys.spotify);
 
@@ -10,8 +14,8 @@ var searchType = process.argv[2];
 
 var userInput = "";
 
-for (let i = 2; i < process.argv.length; i++) {
-    if (i > 2) {
+for (let i = 3; i < process.argv.length; i++) {
+    if (i > 3) {
         userInput = userInput + "+" + process.argv[i];
     } else {
         userInput = process.argv[i];
@@ -28,25 +32,40 @@ var urlOMBD = "http://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=
 
 
 if (searchType === "concert-this") {
-    console.log("venue");
-    console.log("location");
-    console.log("date")
+    var urlConcerts = "https://rest.bandsintown.com/artists/" + userInput + "/events?app_id=codingbootcamp";
+    axios.get(urlConcerts).then(
+        function(response) {
+            console.log("Venue: " + response.data[0].venue.name);
+            console.log("Location: " + response.data[0].venue.city + ", " + response.data[0].venue.region + " " + response.data[0].venue.country);
+            console.log("Date: " + moment(response.data[0].datetime).format('L'));
+            // console.log("venue");
+            // console.log("location");
+            // console.log("date")
+        })
 } else if (searchType === "spotify-this-song") {
-    if (process.argv.length < 3) {
-        userInput = "The+Sign"
-    }
-    spotify.search({ type: 'track', query: userInput }, function(err, data) {
+    if (process.argv.length === 3) {
+        spotify.request('https://api.spotify.com/v1/tracks/0hrBpAOgrt8RXigk83LLNE')
+        .then(function(data) {
+        console.log("Artist: " + data.artists[0].name);
+        console.log("Song: " + data.name);
+        console.log("Preview: " + data.preview_url);
+        console.log("Album: " + data.album.name);
+        // console.log(data); 
+        })
+        .catch(function(err) {
+        console.error('Error occurred: ' + err); 
+        });
+    } else {
+    spotify.search({ type: 'track', query: userInput, limit: 1 }, function(err, data) {
         if (err) {
             return console.log('Error occurred: ' + err);
         }
-        
-        console.log(data); 
+        console.log("Artist: " + data.tracks.items[0].artists[0].name);
+        console.log("Song: " + data.tracks.items[0].name);
+        console.log("Preview: " + data.tracks.items[0].preview_url);
+        console.log("Album: " + data.tracks.items[0].album.name);
     });
-    
-    console.log("artist");
-    console.log("song name");
-    console.log("preview link");
-    console.log("album");
+    }
 
 } else if (searchType === "movie-this") {
     if (process.argv.length < 3) {
